0e6fdefde448721fabb269890c78253d8170d85f
cjaws <cjaworsk@gmail.com>
2025-09-13 20:09:41 -0700

    changing to 100dvh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const MapNoSSR = dynamic(() => import("../components/Map"), { ssr: false });
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-screen">
+    <div className="relative h-[100dvh] w-screen">
       <MapNoSSR />
     </div>
   );
